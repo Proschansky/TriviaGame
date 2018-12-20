668c544af68a37a533463a55ca88950e53b52a14
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2018-12-19 20:27:41 -0500

    It now picks a random question and poses it.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,62 +1,67 @@
-// var userPick;
+var userPick;
 
-// var correctAnswer = 0;
+var correctAnswer = 0;
 
-// var incorrectAnswer = 0;
+var incorrectAnswer = 0;
 
-// var unAnswer = 0;
+var unAnswer = 0;
 
-// var question = 0;
+var question = 0;
 
-// var images;
+var images;
 
-// var counter=30;
+var counter=30;
 
-// var disneyQuestion = [{
-//     question: "In Aladdin, what is the name of Jasmine's pet tiger?",
-//     choices: ["Rajah", "Bo", "Iago", "Jack" ],
-//     images:  ["../images/Rajah.gif"],
-//     validAnswer: 0
-//     }, {
-//     question:"In Peter Pan, Captain Hook had a hook on which part of his     body?",
-//     choices: ["Right Foot", "Left Hand", "Left Foot", "Right Hand"],
-//     validAnswer: 1
+var disneyQuestion = [{
+    question: "In Aladdin, what is the name of Jasmine's pet tiger?",
+    choices: ["Rajah", "Bo", "Iago", "Jack" ],
+    images:  ["../images/Rajah.gif"],
+    validAnswer: 0
+    }, {
+    question:"In Peter Pan, Captain Hook had a hook on which part of his     body?",
+    choices: ["Right Foot", "Left Hand", "Left Foot", "Right Hand"],
+    validAnswer: 1
     
-//     }, {
-//     question:"In the Lion King, where does Mufasa and his family live?",
-//     choices: ["Rocky Mountain", "Forest", "Desert", "Pride Rock"],
-//     validAnswer: 3
+    }, {
+    question:"In the Lion King, where does Mufasa and his family live?",
+    choices: ["Rocky Mountain", "Forest", "Desert", "Pride Rock"],
+    validAnswer: 3
     
-//     }, {
-//     question:"In Beauty and the Beast, how many eggs does Gaston eat for    breakfast?",
-//     choices: ["2 Dozen", "5 Dozen", "5000", "0"],
-//     validAnswer: 1
+    }, {
+    question:"In Beauty and the Beast, how many eggs does Gaston eat for breakfast?",
+    choices: ["2 Dozen", "5 Dozen", "5000", "0"],
+    validAnswer: 1
     
-//     }, {
-//     question:"In Alice in Wonderland, what is the name of Alice’s kitten?",
-//     choices: ["Dinah", "Sammie", "Kat", "Luna"],
-//     validAnswer: 0
+    }, {
+    question:"In Alice in Wonderland, what is the name of Alice’s kitten?",
+    choices: ["Dinah", "Sammie", "Kat", "Luna"],
+    validAnswer: 0
     
-//      }, {
-//     question:"After being on earth, where did Hercules first meet his   father Zeus?",
-//     choices: ["Mount Olympus", "Greece", "In the Temple of Zeus", "Elysian   Fields"],
-//     validAnswer: 2
+     }, {
+    question:"After being on earth, where did Hercules first meet his   father Zeus?",
+    choices: ["Mount Olympus", "Greece", "In the Temple of Zeus", "Elysian   Fields"],
+    validAnswer: 2
     
-//     }, {
-//     question:"During the ballroom scene of Beauty & the Beast, what color is Belle’s Gown?",
-//     choices: ["Yellow", "Blue", "Gold", "White"],
-//     validAnswer: 2
+    }, {
+    question:"During the ballroom scene of Beauty & the Beast, what color is Belle’s Gown?",
+    choices: ["Yellow", "Blue", "Gold", "White"],
+    validAnswer: 2
     
-//     }, {
-//     question:"In Bambi, what word does the owl use to describe falling in love?",
-//     choices: ["Whimsical", "Miserable", "Joyful", "Twitterpatted"],
-//     validAnswer: 3
+    }, {
+    question:"In Bambi, what word does the owl use to describe falling in love?",
+    choices: ["Whimsical", "Miserable", "Joyful", "Twitterpatted"],
+    validAnswer: 3
     
-//     }
+    }
     
-//     ];
+    ];
 
-    // console.log(disneyQuestion[0].question);
+    console.log(disneyQuestion.length);
+
+function shuffle(o) {
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+    };
 
 var number = 30; //  Set our number counter to 100.
 
@@ -69,15 +74,18 @@ function reset() {
    $("startOver").attr("id", "startButton");
    $("#startButton").text("Start Over");
    number = 30;
+   shuffle(disneyQuestion)
    intervalId;
 };
 
 //  The run function sets an interval that runs the decrement function once a second.
 function run() {
+    disneyQuestion = shuffle(disneyQuestion)
    $("#startButton").attr('id', 'startOver');
    $("startOver").text("Start Over");
    $("#spaceTitle").empty();
-   $("#startButton").hide();
+   $(".button").hide();
+   $("#showNumber").html("<h2>"+ shuffle(disneyQuestion[0].question))
    clearInterval(intervalId);
    intervalId = setInterval(decrement, 1000);
 }
